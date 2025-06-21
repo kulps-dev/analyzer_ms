9399b23223287b923824ac9123c42073f1512730
KULPS <kulpin.olexander@yandex.ru>
2025-06-21 21:06:38 +0300

    внес доп  правки33

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import psycopg2
-from .moysklad import MoyskladAPI
+from moysklad import MoyskladAPI
 from datetime import datetime
 import os
 from openpyxl import Workbook
